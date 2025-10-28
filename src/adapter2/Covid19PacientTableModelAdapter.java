d9b17deb43281929c1122268ea0d979696c18d37
etxeg <etxeetxe1019@gmail.com>
2025-10-28 10:58:01 +0100

    Adapter bukatuta

--- src/adapter2/Covid19PacientTableModelAdapter.java
+++ src/adapter2/Covid19PacientTableModelAdapter.java
@@ -1,35 +1,49 @@
 package adapter2;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import javax.swing.table.AbstractTableModel;
 
 import domain.Covid19Pacient;
+import domain.Symptom;
+import iterator.Covid19PacientIterator;
 
 public class Covid19PacientTableModelAdapter extends AbstractTableModel {
-	  protected Covid19Pacient pacient;
-	  protected String[] columnNames =
-	    new String[] {"Symptom", "Weight" };
+	protected Covid19Pacient pacient;
+	protected String[] columnNames = new String[] { "Symptom", "Weight" };
 
-	  public Covid19PacientTableModelAdapter(Covid19Pacient p) {
-	    this.pacient=p;
-	  }
-
-	  public int getColumnCount() {
-	    // Challenge!
-		 return 1;
-	  }
-
-	  public String getColumnName(int i) {
-	    // Challenge!
-		  return "Column name 1";
-	  }
-
-	  public int getRowCount() {
-	    // Challenge!
-		  return 1;
-	  }
-
-	  public Object getValueAt(int row, int col) {
-	    // Challenge!
-		  return "value";
-	  }
+	public Covid19PacientTableModelAdapter(Covid19Pacient p) {
+		this.pacient = p;
 	}
+
+	public int getColumnCount() {
+		// Challenge!
+		return 2;
+	}
+
+	public String getColumnName(int i) {
+		return columnNames[i];
+	}
+
+	public int getRowCount() {
+		return pacient.getSymptoms().size();
+	}
+
+	public Object getValueAt(int row, int col) {
+		Covid19PacientIterator it = (Covid19PacientIterator) pacient.iterator();
+		ArrayList<Symptom> symptomList= new ArrayList<Symptom>();
+		while (it.hasNext()) {
+			symptomList.add((Symptom) it.next());
+			
+		}
+		
+		if (col == 0) {
+			return symptomList.get(row).getName();
+		} else if (col==1) {
+			return pacient.getWeight(symptomList.get(row));
+		}
+		return null;
+	}
+}
--- src/adapter2/Main.java
+++ src/adapter2/Main.java
@@ -6,15 +6,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		Covid19Pacient pacient=new Covid19Pacient("aitor", 35);
+		Covid19Pacient p2 = new Covid19Pacient("mikel", 25);
 		
 		pacient.addSymptomByName("disnea", 2);
 		pacient.addSymptomByName("cefalea", 1);
 		pacient.addSymptomByName("astenia", 3);
+		p2.addSymptomByName("disnea", 1);
+		p2.addSymptomByName("cefalea", 3);
 		
 		ShowPacientTableGUI gui=new ShowPacientTableGUI(pacient);
 		gui.setPreferredSize(
 			      new java.awt.Dimension(300, 200));
 		gui.setVisible(true);
+		
+		ShowPacientTableGUI gui2 = new ShowPacientTableGUI(p2);
+		gui2.setPreferredSize(
+				new java.awt.Dimension(300, 200));
+		gui2.setVisible(true);
 
 
 	}
